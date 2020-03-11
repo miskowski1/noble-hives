308fb456e5d7146c8c15a4ab215fb074e8092383
miskowski1 <miskowski1@gmail.com>
2020-03-11 16:17:24 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCcNF_3cOTe1lTG7gSZWlhyvMFWr-4vix4',
+    authDomain: 'noblehives.firebaseapp.com',
+    databaseURL: 'https://noblehives.firebaseio.com',
+    projectId: 'noblehives',
+    storageBucket: 'noblehives.appspot.com',
+    messagingSenderId: '320105046968',
+    appId: '1:320105046968:web:41306dacdfa857197fe54d'
+  }
 };
