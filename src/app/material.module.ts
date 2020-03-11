6022b8f0359874d73dccbc6edb73d4d4d6296a66
miskowski1 <miskowski1@gmail.com>
2020-03-11 11:08:43 +0100

    First commit

---
+++ src/app/material.module.ts
@@ -0,0 +1,11 @@
+import {NgModule} from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
+@NgModule({
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule],
+  exports: [MatCardModule, MatFormFieldModule, MatInputModule]
+})
+export class MaterialModule {
+}
